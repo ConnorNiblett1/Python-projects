575337f3b18f404f27ea826ff388d6011110833b
ConnorNiblett1 <149905183+ConnorNiblett1@users.noreply.github.com>
2023-11-06 13:37:04 -0600

    Create csv_sort.py

---
+++ csv_sort.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('country-list.csv', 'r') as csvfile:
+   reader = csv.reader(csvfile)
+   sorted_rows = sorted(reader, key=lambda row: row[0])
+   
+   for row in sorted_rows:
+       print(row)
